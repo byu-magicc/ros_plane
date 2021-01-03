a5edf15385be2cc19ab6a4ea262d0b6beec9790e
kpiyush25 <piyushk@iitk.ac.in>
2021-01-03 22:26:11 +0530

    Modularize the code

--- rosplane/src/path_planner.cpp
+++ rosplane/src/path_planner.cpp
@@ -1,106 +1,42 @@
-#include <ros/ros.h>
-#include <rosplane_msgs/Waypoint.h>
-#include <visualization_msgs/MarkerArray.h>
+#include <path_planner.h>
 
-#define num_waypoints 33  // kept 33(8*4 + 1) initially
+void RosplaneDubins::init(ros::NodeHandle& nh) {
+    waypointPublisher = nh.advertise<rosplane_msgs::Waypoint>("waypoint_path", 10);
+    visualize_points_pub = nh.advertise<visualization_msgs::MarkerArray>("visualize_points", 10);
 
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "rosplane_simple_path_planner");
+    nh.param<double>("R_min", r, 25.0);
+    nh.param<double>("launch_position_x", start_x, -5.0);
+    nh.param<double>("left_pylon_y", left_y, 8.66);
+    nh.param<double>("right_pylon_y", right_y, 408.66);
+    nh.param<double>("height", height, -20);
+    nh.param<float>("Va", Va, 2.0);
+    nh.param<int>("num_waypoints", num_waypoints, 33);
 
-    ros::NodeHandle nh_;
-    ros::Publisher waypointPublisher = nh_.advertise<rosplane_msgs::Waypoint>("waypoint_path", 10);
-    ros::Publisher visualize_points_pub = nh_.advertise<visualization_msgs::MarkerArray>("visualize_points", 10);
+    wps.resize(5 * num_waypoints);
+    RosplaneDubins::computeTangencyPoints();
+    RosplaneDubins::computePoints();
+}
 
-    double start_x, left_y, right_y, r, height;
-    double hunter_killer_x, hunter_killer_y;
-    nh_.param<double>("R_min", r, 25.0);
-    nh_.param<double>("launch_position_x", start_x, -5.0);
-    nh_.param<double>("left_pylon_y", left_y, 8.66);
-    nh_.param<double>("right_pylon_y", right_y, 408.66);
-    nh_.param<double>("height", height, -20);
+void RosplaneDubins::computeTangencyPoints() {
+    double l = sqrt(start_x * start_x + left_y * left_y);  // launch position to left pylon distance
+    double zen_angle = acos(left_y / l);                   // angle between y-axis and line joining launch position and pylon
+    tangency_angle_ = acos(r / l) - zen_angle;
+    loop_entry_point_x = r * sin(tangency_angle_);
+    loop_entry_point_y = left_y - r * cos(tangency_angle_);
+    loop_exit_point_x = -r * sin(tangency_angle_);
+    loop_exit_point_y = left_y - r * cos(tangency_angle_);
+}
 
+void RosplaneDubins::computePoints() {
     hunter_killer_x = -start_x;
     hunter_killer_y = 0.0;
-
-    double l = sqrt(start_x * start_x + left_y * left_y);  // launch position to left pylon distance
-    double zen_angle = acos(left_y / l);                   // angle between y-axis and line joining launch and pylon
-    double tangency_angle_ = acos(r / l) - zen_angle;
-    double loop_entry_point_x = r * sin(tangency_angle_);
-    double loop_entry_point_y = left_y - r * cos(tangency_angle_);
-    double loop_exit_point_x = -r * sin(tangency_angle_);
-    double loop_exit_point_y = left_y - r * cos(tangency_angle_);
-
-    float Va;
-    nh_.param<float>("Va", Va, 2.0);
-    float wps[5 * num_waypoints];
-    //  = {
-    // 17.3143, 40.6263, -20, -0.765049, Va,
-    // these were the points that try to generate a figure like the actual problem statement..launch pos was -40.0, r_min was 25.0, left_y was 48.66,
-    // right_y was 108.66
-    //     loop_entry_point_x,
-    //     loop_entry_point_y,
-    //     height,
-    //     -tangency_angle_,
-    //     Va,
-    //     -r,
-    //     right_y,
-    //     height,
-    //     M_PI / 2,
-    //     Va,
-    //     r,
-    //     left_y,
-    //     -20,
-    //     -M_PI / 2,
-    //     Va,
-    //     -25,
-    //     108.66,
-    //     -20,
-    //     -M_PI / 2,
-    //     Va,
-    //     11.4256,
-    //     26.4236,
-    //     -20,
-    //     0,
-    //     Va,
-    //     -25,
-    //     108.66,
-    //     -20,
-    //     -M_PI / 2,
-    //     Va,
-    //     11.4256,
-    //     26.4236,
-    //     -20,
-    //     0,
-    //     Va,
-    //     -25,
-    //     108.66,
-    //     -20,
-    //     -M_PI / 2,
-    //     Va,
-
-    //     // 0, 200, -20, 45 * M_PI / 180, Va, 200, 200, -20, 225 * M_PI / 180, Va,
-    // 200,
-    // 0,
-    // -50,
-    // 45 * M_PI / 180,
-    // Va,
-    // 0,
-    // 200,
-    // -50,
-    // 45 * M_PI / 180,
-    // Va,
-    // 200,
-    // 200,
-    // -50,
-    // 225 * M_PI / 180,
-    // Va,
-    // };
-
+    int i = 0;
     visualization_msgs::MarkerArray marker_array;
     visualization_msgs::Marker marker;
     marker.type = visualization_msgs::Marker::ARROW;
     marker.action = visualization_msgs::Marker::ADD;
     marker.header.frame_id = "base_link";
+    marker.header.stamp = ros::Time();
     marker.pose.position.x = 0;
     marker.pose.position.y = 0;
     marker.pose.position.z = 0;
@@ -116,8 +52,6 @@ int main(int argc, char** argv) {
     marker.color.g = 0.0;
     marker.color.b = 1.0;
     marker.lifetime = ros::Duration(0);
-
-    int i = 0;
     for (int j = 0; j < (num_waypoints - 1) / 4; j++) {
         if (!j) {
             wps[20 * i] = loop_entry_point_x;
@@ -204,7 +138,6 @@ int main(int argc, char** argv) {
             marker.pose.orientation.w = cos(M_PI / 2);
             marker_array.markers.push_back(marker);
         }
-
         i++;
     }
     wps[20 * i] = hunter_killer_x;
@@ -219,8 +152,12 @@ int main(int argc, char** argv) {
     marker.pose.orientation.y = -sin(tangency_angle_ / 2);
     marker.pose.orientation.z = -sin(tangency_angle_ / 2);
     marker.pose.orientation.w = cos(tangency_angle_ / 2);
+    visualize_points_pub.publish(marker);
+    ros::Duration(0.5).sleep();
     marker_array.markers.push_back(marker);
+}
 
+void RosplaneDubins::run() {
     for (int i(0); i < num_waypoints; i++) {
         ros::Duration(0.5).sleep();
 
@@ -247,6 +184,78 @@ int main(int argc, char** argv) {
         visualize_points_pub.publish(marker_array);
         ros::Duration(0.5).sleep();
     }
-
-    return 0;
 }
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "rosplane_simple_path_planner");
+    ros::NodeHandle nh;
+    RosplaneDubins trajectory;
+
+    trajectory.init(nh);
+    trajectory.run();
+}
+
+/*
+float wps[5 * num_waypoints] = {
+        // 17.3143, 40.6263, -20, -0.765049, Va,
+        // these were the points that try to generate a figure like the actual problem statement..launch pos was -40.0, r_min was 25.0, left_y was 48.66,
+        // right_y was 108.66
+        11.4256,
+        26.4236,
+        -20,
+        0.426903,
+        Va,
+        -25,
+        108.66,
+        -20,
+        -M_PI / 2,
+        Va,
+        11.4256,
+        26.4236,
+        -20,
+        0,
+        Va,
+        -25,
+        108.66,
+        -20,
+        -M_PI / 2,
+        Va,
+        11.4256,
+        26.4236,
+        -20,
+        0,
+        Va,
+        -25,
+        108.66,
+        -20,
+        -M_PI / 2,
+        Va,
+        11.4256,
+        26.4236,
+        -20,
+        0,
+        Va,
+        -25,
+        108.66,
+        -20,
+        -M_PI / 2,
+        Va,
+
+        //     // 0, 200, -20, 45 * M_PI / 180, Va, 200, 200, -20, 225 * M_PI / 180, Va,
+        // 200,
+        // 0,
+        // -50,
+        // 45 * M_PI / 180,
+        // Va,
+        // 0,
+        // 200,
+        // -50,
+        // 45 * M_PI / 180,
+        // Va,
+        // 200,
+        // 200,
+        // -50,
+        // 225 * M_PI / 180,
+        // Va,
+    };
+*/
