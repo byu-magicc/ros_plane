d21016b496a08ab39a0b9ab5850dfc077c2dd0e6
kpiyush25 <piyushk@iitk.ac.in>
2021-01-03 22:25:21 +0530

    Add header file for path_planner.cpp

---
+++ rosplane/include/path_planner.h
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+#include <rosplane_msgs/Waypoint.h>
+#include <vector>
+#include <visualization_msgs/MarkerArray.h>
+
+class RosplaneDubins {
+  public:
+    void init(ros::NodeHandle& nh);
+    void run();
+
+  private:
+    void computeTangencyPoints();
+    void computePoints();
+
+    double start_x;
+    double left_y;
+    double right_y;
+    double r;
+    double height;
+    double hunter_killer_x;
+    double hunter_killer_y;
+    double tangency_angle_;
+    double loop_entry_point_x;
+    double loop_entry_point_y;
+    double loop_exit_point_x;
+    double loop_exit_point_y;
+    float Va;
+    int num_waypoints;
+
+    std::vector<float> wps;
+
+    visualization_msgs::MarkerArray marker_array;
+    ros::Publisher waypointPublisher;
+    ros::Publisher visualize_points_pub;
+};
